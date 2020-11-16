36a54d7f3fee6d3c307149554ae7debb8332f017
ohamjoseph <ouilyh@gmail.com>
2020-11-16 15:19:27 +0000

    creation de nouveau cards

--- src/components/card_items.js
+++ src/components/card_items.js
@@ -5,23 +5,20 @@ import '../css/card_item.css';
 
 const CardExample = () => {
   return (
-    <div class="card_item">
-        <img class="card-img-top" src={"./assets/img/carousel/placeholder.png"} alt="Card image cap" class="img-thumbnail"/>
-        
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                </div>
-            </li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-        
-        <div class="card-footer bg-dark">
-            <small class="text-white">Plus...</small>
+    <div className="card mb-3">
+    <div className="row no-gutters">
+      <div className="col-6 col-md-6 col-lg-4 ">
+        <img className="card-img-top" src={"./assets/img/photos/index2.png"} alt="Card image cap" className="img-thumbnail"/>
+      </div>
+      <div className="col-6 col-md-6 col-lg-8">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">lead-in to additional content. This content is a little bit longer.</p>
+          <p className="card-text"><small className="text-muted">Plus...</small></p>
         </div>
+      </div>
     </div>
+  </div>
   )
 }
 
